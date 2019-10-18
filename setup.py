e4271ece396c72eac12d1db0a639bb8cbab20f20
Jarosław Skrzypek <skrzypek.jarek@gmail.com>
2019-10-18 12:55:31 +0200

    initial version

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2019 by Jarosław Skrzypek. All rights reserved.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import setuptools
+
+setuptools.setup(
+     name='loudds',
+     version='0.1',
+     author="Jarosław Skrzypek",
+     author_email="skrzypek.jarek@gmail.com",
+     description="Simply send predictions from FastAI Learner to HTTP(s) endpoint",
+     url="https://github.com/jareks/loudds",
+     packages=setuptools.find_packages(),
+     classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Development Status :: 3 - Alpha",
+     ],
+ )
