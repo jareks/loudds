6dd83eaa1a79c61b92b0de07dca1a2d466cd6ace
Jarosław Skrzypek <skrzypek.jarek@gmail.com>
2020-01-13 11:34:53 +0100

    document sending access token

--- loudds/fastai/fastai.py
+++ loudds/fastai/fastai.py
@@ -15,40 +15,56 @@ learn = fastai.vision.cnn_learner(data, model, metrics=error_rate)
 # train model...
 
 summary = loud.prepare_summary(learn, { DatasetType.Valid: 'validation' })
-requests.post("http://1.2.3.4/endpoint", json=summary)
+requests.post("http://1.2.3.4/endpoint", json=summary, headers={'Authorization': f"bearer {LOUDDATA_TOKEN}"})
 """
 
 from fastai.basic_data import DatasetType, DeviceDataLoader
 from fastai.vision import Learner
 from torch import Tensor
 
-def _summary_part(classes: list, predictions: Tensor, targets: Tensor, losses: Tensor,
-                  dataset: DeviceDataLoader, dataset_name: str):
+
+def _summary_part(
+    classes: list,
+    predictions: Tensor,
+    targets: Tensor,
+    losses: Tensor,
+    dataset: DeviceDataLoader,
+    dataset_name: str,
+):
     """ Returns summary of predictions against targets """
 
     items = []
     for i in range(len(dataset.items)):
-      filename = dataset.items[i].name
-      items.append({
-          'predictions': { classes[j]: float(predictions[i][j]) for j in range(len(classes)) },
-          'target': classes[targets[i]],
-          'loss': losses[i].tolist(),
-          'filename': filename,
-          'type': dataset_name
-      })
+        filename = dataset.items[i].name
+        items.append(
+            {
+                "predictions": {
+                    classes[j]: float(predictions[i][j]) for j in range(len(classes))
+                },
+                "target": classes[targets[i]],
+                "loss": losses[i].tolist(),
+                "filename": filename,
+                "type": dataset_name,
+            }
+        )
     return items
 
-def prepare_summary(learner: Learner, sets: dict = { DatasetType.Valid: 'validation', DatasetType.Test: 'test' }):
+
+def prepare_summary(
+    learner: Learner,
+    sets: dict = {DatasetType.Valid: "validation", DatasetType.Test: "test"},
+):
     """ Returns summary of predictions made by learner """
 
     classes = learner.data.classes
-    summary = {
-        'items': []
-    }
+    summary = {"items": []}
 
     for ds_type, name in sets.items():
-        predictions, targets, losses = learner.get_preds(ds_type=ds_type, with_loss=True)
+        predictions, targets, losses = learner.get_preds(
+            ds_type=ds_type, with_loss=True
+        )
         dataset = learner.data.dl(ds_type)
-        summary['items'] += (_summary_part(classes, predictions, targets, losses, dataset, name))
+        summary["items"] += _summary_part(
+            classes, predictions, targets, losses, dataset, name
+        )
     return summary
-
